b0dc88281cf460eac94959641dfbbbeb578e1163
Meezaan Davids <meezaandavids365@gamil.com>
2022-11-09 09:38:56 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,27 +1,27 @@
 // Addition
-function addition() {
-    console.log(2+2);
-}
+// function addition() {
+//     console.log(2+2);
+// }
 // Subtraction
-function subtraction() {
-    console.log(5-2);
-}
+// function subtraction() {
+//     console.log(5-2);
+// }
 // Multiplication
-function multiplication() {
-    console.log(3*3);
-}
+// function multiplication() {
+//     console.log(3*3);
+// }
 // Division
-function division() {
-    console.log(5/2);
-}
+// function division() {
+//     console.log(5/2);
+// }
 // Exponents
-function exponents() {
-    console.log(2**2);
-}
+// function exponents() {
+//     console.log(2**2);
+// }
 // Modulus
-function modulus() {
-    console.log(5%2);
-}
+// function modulus() {
+//     console.log(5%2);
+// }
 // Call all functions
 // addition();
 // subtraction();
@@ -41,3 +41,33 @@ function modulus() {
 // numb1 += 2;
 // console.log(numb1);
 
+// First approach: make use of function 
+// let firstName = prompt("Please enter your name:");
+// let surname = prompt("Please enter your surname:");
+// let salary = prompt("Please enter your salary: ", 0);
+// document.write(`
+// Name:${firstName}<br>
+// Surname:${surname}<br>
+// Salary:R${eval(`${salary}*2`)}`);
+
+// My Attempt
+// let firstName = prompt("Please enter your firstname:");
+// let lastName = prompt("Please enter your lastname:");
+// let salary = prompt("Please enter your salary:")
+
+// document.write(`
+// Name:${firstName}<br>
+// Lastname:${lastName}<br>
+// Salary:R${eval(`${salary}*5`)}`);
+
+
+// function addition(){
+//     return 9 + 2;
+// }
+// document.write(`Sum: ${addition()}`);
+
+let addition = ()=>{
+    document.write(8 + 4);
+}
+
+addition();
