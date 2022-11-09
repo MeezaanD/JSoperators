1d5753d048dead6a8dc7b4efb89cb75cc6a9514c
Meezaan Davids <meezaandavids365@gamil.com>
2022-11-09 08:42:06 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,43 @@
+// Addition
+function addition() {
+    console.log(2+2);
+}
+// Subtraction
+function subtraction() {
+    console.log(5-2);
+}
+// Multiplication
+function multiplication() {
+    console.log(3*3);
+}
+// Division
+function division() {
+    console.log(5/2);
+}
+// Exponents
+function exponents() {
+    console.log(2**2);
+}
+// Modulus
+function modulus() {
+    console.log(5%2);
+}
+// Call all functions
+// addition();
+// subtraction();
+// multiplication();
+// division();
+// exponents();
+// modulus();
+
+// Assignments
+// let numb1 = 4;
+// let numb2 = 6;
+// let result = 0;
+// result += numb1 + numb2;
+// console.log(result);
+
+// let numb1 = 3;
+// numb1 += 2;
+// console.log(numb1);
+
